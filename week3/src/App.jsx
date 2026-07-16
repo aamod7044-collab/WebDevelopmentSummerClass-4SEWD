872ce9f59289f36b96c99af3838b0627cbe77c37
Aamod Timilsina <aamod7044@gmail.com>
2026-07-16 12:38:32 +0545

    Week 4

---
+++ week3/src/App.jsx
@@ -0,0 +1,13 @@
+import PageTitle from "./component/PageTitle/PageTitle";
+import TaskComponent from "./component/PageTitle/Task Component/TaskComponent";
+
+function App() {
+  return (
+    <>
+      <PageTitle />
+      <TaskComponent />
+    </>
+  );
+}
+
+export default App;
